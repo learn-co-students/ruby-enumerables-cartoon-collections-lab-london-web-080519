0abdd84981f00098ba09048fc24cae44f5d02549
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 14:13:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,14 +10,22 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map do |element|
-    element.capitalize
+    element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? do |element|
+    element.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find do |element|
+    cheese_types.include?(element)
+  end
 end
