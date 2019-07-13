fe337eb07e73a99e1e298d848b14ed68ebe58c94
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 14:08:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  i = 1
+  array.each do |element|
+    puts "#{i}. #{element}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |element|
+    element.capitalize
 end
 
 def long_planeteer_calls# code an argument here
